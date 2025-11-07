b1042ad56d731d01c5fab11778aaae9302b5db09
Dominique Saputro <dominique.saputro@gmail.com>
2025-11-07 11:12:29 +0700

    [FIX] Url from API too long

--- streamlit/home.py
+++ streamlit/home.py
@@ -39,6 +39,7 @@ token = components.html(
 # token = query_params.get("token", [None])[0] if isinstance(query_params.get("token"), list) else query_params.get("token")
 
 if token:
+    st.write(token)
     st.session_state["token"] = token
     status_placeholder = st.empty()
     status_placeholder.info("Validating token with Coretax API...")
