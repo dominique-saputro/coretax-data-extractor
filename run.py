370044c0d7fd61f2f7d634b206478ec46132948d
Dominique Saputro <101784914+dominique-saputro@users.noreply.github.com>
2026-05-13 12:12:11 +0700

    Add run.py to launch Streamlit application

---
+++ run.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+import os
+
+if __name__ == "__main__":
+    app_path = os.path.join("streamlit", "home.py")
+
+    subprocess.run([
+        sys.executable,
+        "-m",
+        "streamlit",
+        "run",
+        app_path
+    ])
